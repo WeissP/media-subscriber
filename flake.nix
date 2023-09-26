452c261150e9d8d42dc1e2869adda193b7d5a588
weiss <kingbaiing@163.com>
2023-09-26 22:18:28 +0800

    add multiple dev shells

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A devShell example";
+  description = "Media Subscriber";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -12,21 +12,36 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
+        SERVER_SECRET =
+          "2a4855f2c950a0e6167d80059e657647b70231529bec3015a56188a387f956e8cdf6277e06f5a4c335e9782971b984e3feb600f4a19acf536240f51a16560575";
+        SERVER_PORT = "7070";
+        RUST_LOG = "debug";
+        commonInputs = with pkgs; [
+          openssl
+          pkg-config
+          rust-bin.nightly."2023-07-04".default
+          nodejs_20
+        ];
       in with pkgs; {
-        devShells.default = mkShell {
-          SERVER_SECRET =
-            "2a4855f2c950a0e6167d80059e657647b70231529bec3015a56188a387f956e8cdf6277e06f5a4c335e9782971b984e3feb600f4a19acf536240f51a16560575";
-          SERVER_PORT = "7070";
-          RUST_LOG = "debug";
-          buildInputs = [
-            openssl
-            pkg-config
-            rust-analyzer
-            rust-bin.nightly."2023-07-04".default
-            cargo-limit
-            nodejs_20
-            nodePackages.svelte-language-server
-          ];
+        devShells = {
+          default = mkShell {
+            inherit SERVER_SECRET SERVER_PORT RUST_LOG;
+            buildInputs = commonInputs;
+          };
+          backend = mkShell {
+            inherit SERVER_SECRET SERVER_PORT RUST_LOG;
+            buildInputs = commonInputs ++ [
+              cargo-limit
+              yt-dlp
+              rust-analyzer
+              nodePackages.svelte-language-server
+            ];
+          };
+          frontend = mkShell {
+            inherit SERVER_SECRET SERVER_PORT RUST_LOG;
+            buildInputs = commonInputs
+              ++ [ nodePackages.svelte-language-server ];
+          };
         };
       });
 }
