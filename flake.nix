ff8082b86c4e5ed50dad2db628215aee56eae7d0
weiss <kingbaiing@163.com>
2023-09-27 07:37:58 +0800

    add darwin specific deps

--- flake.nix
+++ flake.nix
@@ -16,12 +16,17 @@
           "2a4855f2c950a0e6167d80059e657647b70231529bec3015a56188a387f956e8cdf6277e06f5a4c335e9782971b984e3feb600f4a19acf536240f51a16560575";
         SERVER_PORT = "7070";
         RUST_LOG = "debug";
-        commonInputs = with pkgs; [
-          openssl
-          pkg-config
-          rust-bin.nightly."2023-07-04".default
-          nodejs_20
-        ];
+        commonInputs = with pkgs;
+          [ openssl pkg-config rust-bin.nightly."2023-07-04".default nodejs_20 ]
+          ++ (if stdenv.isDarwin then
+            with darwin.apple_sdk.frameworks; [
+              IOKit
+              Security
+              CoreServices
+              SystemConfiguration
+            ]
+          else
+            [ ]);
       in with pkgs; {
         devShells = {
           default = mkShell {
